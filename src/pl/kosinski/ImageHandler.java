c678c8b61c58f5e4c07fedf5e7090d376d7a47a9
kosinski1215 <konrad@konrad-pc.AsusRuter>
2015-03-26 21:26:49 +0100

    błąd w Image Handler

--- src/pl/kosinski/ImageHandler.java
+++ src/pl/kosinski/ImageHandler.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
@@ -13,26 +14,40 @@ import javax.swing.JPanel;
 
 public class ImageHandler extends JPanel {
 	private BufferedImage obraz = null;
+	private BufferedImage obraz2 = null;
 	public ImageHandler() {
 		super();
 		
 		try {
-			obraz = ImageIO.read(new File("C:\\Users\\konrad\\Desktop\\IMG_20150325_151356972.jpg"));
+			obraz = ImageIO.read(new File("C:\\Users\\konrad\\Desktop\\IMG_20150325_151143203.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Dimension dimension = new Dimension(obraz.getWidth()/2, obraz.getHeight()/2);
 		setPreferredSize(dimension);
+		int[][] obraz_tab = new int[obraz.getWidth()][obraz.getHeight()];
 		
+		for(int i = 0; i < obraz.getWidth();i++){
+			for(int j = 0; j < obraz.getHeight();j++){
+				obraz_tab[i][j] = obraz.getRGB(i,j);
+			}
+		}
+
+		//NIE DZIA£A
+		BufferedImage obraz2 = new BufferedImage(obraz.getWidth(),obraz.getHeight(),obraz.getType());
+		for(int i = 0; i < obraz2.getWidth();i++){
+			for(int j = 0; j < obraz2.getHeight();j++){
+				obraz2.setRGB(i,j,obraz_tab[i][j]);
+			}
+		}
 		
 	}
-	@Override
+	
 	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		AffineTransform at = new AffineTransform();
 		at.scale(0.5,0.5);
-		
 		g2d.drawImage(obraz,at,this);
 	}
 	
--- src/pl/kosinski/Obraz.java
+++ src/pl/kosinski/Obraz.java
@@ -7,14 +7,14 @@ public class Obraz {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
+		//EventQueue.invokeLater(new Runnable() {
+			
+		//	public void run() {
 				new Okno();
-			}
+		//	}
 			
 			
-		});
+		//});
 		
 		
 		
